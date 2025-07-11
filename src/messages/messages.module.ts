17d108bad47511c4ce0dc88c73fbdd0fa3fb41e9
iamsaintAdrian <adriankimani5@gmail.com>
2025-07-11 16:26:10 +0300

    Added web socket connection

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MessagesController } from './messages.controller';
+import { MessagesGateway } from './messages.gateway';
+import { SendbirdModule } from '../sendbird/sendbird.module';
 
 @Module({
+  imports: [
+    SendbirdModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('jwt.secret'),
+        signOptions: { expiresIn: configService.get('jwt.expiresIn') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [MessagesController],
+  providers: [MessagesGateway],
 })
 export class MessagesModule {}
